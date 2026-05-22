a6ef0377f783e3847ae1161af4ee4968fca92d1a
akhil joy <akhiljoy312@gmail.com>
2026-05-22 11:56:45 +0530

    chore: removing unwanted files

--- Day_13/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".profile").addEventListener("click", () => {
-  document.querySelector(".detail").classList.add("active");
-});
-
-document.querySelector(".close").addEventListener("click", () => {
-  document.querySelector(".detail").classList.remove("active");
-});
